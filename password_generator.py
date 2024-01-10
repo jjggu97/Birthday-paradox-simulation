af40db7845993f2acdaef7462fd63e0ab35b3f31
jjggu97 <jjggu@naver.com>
2024-01-10 19:48:12 +0900

    edit2

--- password_generator.py
+++ password_generator.py
@@ -2,14 +2,28 @@ import random
 import string
 
 def random_password():
-    #set_random_password_length
+    #set random password length
     password_length = random.randint(10, 20)
     
-    #generate_password
+    #generate random password
     characters = string.digits + string.ascii_letters + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(password_length))
     return password
 
-#print_generated_password
+#print generated password
 password = random_password()
 print("password : ", password)
+
+#3 attempts to enter correct password
+for attempt in range(3):
+    entered_password = input("Enter the password: ")
+
+    #check entered password is correct
+    if entered_password == password:
+        print("Access Confirmed")
+        break
+    else:
+        print("Incorrect password. Attempts left:", 2 - attempt)
+#3 fails -> loop ends
+else:
+    print("Access Denied")
